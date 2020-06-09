964e9e6dcbd47af17f1daaa71c0f330eb9643230
Jacob Rozran <jacobrozran@jacobs-air.myfiosgateway.com>
2020-06-09 08:15:45 -0400

    Added logic to keep it cooler during naptime and sleeptime

--- ecobee.R
+++ ecobee.R
@@ -58,7 +58,16 @@ for(sensor in 1:3){
 info$name <- tolower(gsub("'s Room", "", info$name))
 info <- info[info$name == "ellie", ]
 
-info$action <- ifelse(info$temp >= 70, "on", "off")
+current_time <- hour(Sys.time()) + (minute(Sys.time()) / 60)
+is_morning_nap <- ifelse(current_time >= 11.5 & current_time <= 13, TRUE, FALSE)
+is_afternoon_nap <- ifelse(current_time >= 15.5 & current_time <= 18.5, TRUE, FALSE)
+is_sleeptime <- ifelse(current_time >= 22 | current_time <= 10, TRUE, FALSE)
+
+info$action <- ifelse(((is_morning_nap == TRUE | is_afternoon_nap == TRUE | 
+                           is_sleeptime == TRUE) & info$temp >= 68) | 
+                          (!(is_morning_nap == TRUE | is_afternoon_nap == TRUE | 
+                                is_sleeptime == TRUE) & info$temp >= 72), 
+                      "on", "off")
 
 print(paste0(Sys.time(), ": Formatted Data. Telling VeSync what to do."))
 
